914cf4e8103d2b1bad3a8c24d83f6a1d8a344e4b
DashkaPotter <121686163+DashkaPotter@users.noreply.github.com>
2023-07-28 19:09:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,27 +17,27 @@ let getWeather = () => {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
         fetch(url).then((resp)=> resp.json())
         .then((data) =>{
-            console.log(data);
-        console.log(data.weather[0].icon);
-        console.log(data.weather[0].main);
-        console.log(data.weather[0].description);
-        console.log(data.name);
-        console.log(data.main.temp_min);
-        console.log(data.main.temp_max);
+            //console.log(data);
+        //console.log(data.weather[0].icon);
+        //console.log(data.weather[0].main);
+        //console.log(data.weather[0].description);
+        //console.log(data.name);
+        //console.log(data.main.temp_min);
+        //console.log(data.main.temp_max);
         result.innerHTML = `
         <h2>${data.name}</h2>
         <h4 class="weather">${data.weather[0].main}</h4>
         <h4 class="desc">${data.weather[0].description}</h4>
         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1>${data.main.temp} &#176;</h1>
+        <h1>${Math.round(data.main.temp)} &#176;</h1>
         <div class="temp-container">
             <div>
                 <h4 class="title">min</h4>
-                <h4 class="temp">${data.main.temp_min}&#176;</h4>
+                <h4 class="temp">${Math.round(data.main.temp_min)}&#176;</h4>
             </div>
             <div>
                 <h4 class="title">max</h4>
-                <h4 class="temp">${data.main.temp_max}&#176;</h4>
+                <h4 class="temp">${Math.round(data.main.temp_max)}&#176;</h4>
             </div>
         </div>`;
         })
